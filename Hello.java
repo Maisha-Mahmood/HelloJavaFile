93b1b727a4b13574d8d639f8a863bcbe693a4bfe
Maisha-Mahmood <78035756+Maisha-Mahmood@users.noreply.github.com>
2021-01-27 19:32:42 +0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***********************************************
+*Java Program Name
+************************************************
+*Student Name (first and last)
+*Date 
+*Course Number and Section 
+************************************************/
+public class Hello {
+public static void main(String[] args) {
+
+System.out.println("<Your Name>");
+System.out.println("<Java Project Name>");
+System.out.println("<Project Description>");
+System.out.println("<Version Date>");
+System.out.println("<Course Name and Section>");
+System.out.println();
+}
+}
